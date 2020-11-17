3a77c804ad175e2cdb43d68f2532331fa7c841c1
orlann <orlann@ukr.net>
2020-11-17 21:52:40 +0200

    finished unit test 1

---
+++ UnitTestfor5.1/UnitTestfor5.1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "cmath"
+#include "../lab 5.1/5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestfor5
+{
+	TEST_CLASS(UnitTestfor5)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = h(2, 3);
+			Assert::AreEqual(t, -5);
+		}
+	};
+	
+}
